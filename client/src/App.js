249ee57bd1c492efed3a01d56da9757175b858c3
putuduta <putu.duta@gmail.com>
2020-07-25 20:54:53 +0700

    Modify some line

--- client/src/App.js
+++ client/src/App.js
@@ -8,11 +8,13 @@ import './styles/Chat.css';
 import Join from './components/Join';
 import Chat from './components/Chat';
 
-const App = () => (
-    <Router>
-        <Route path="/" exact component={Join} />
-        <Route path="/chat" exact component={Chat} />
-    </Router>
-);
+function App() {
+    return (
+        <Router>
+            <Route path="/" exact component={Join} />
+            <Route path="/chat" exact component={Chat} />
+        </Router>
+    );
+};
 
 export default App;
