b3676d44d2bf054f0d1d7cf0c8bc26964ec827b8
putuduta <putu.duta@gmail.com>
2020-07-28 11:23:41 +0700

    Modify disconnection

--- server/index.js
+++ server/index.js
@@ -38,7 +38,11 @@ io.on('connection', (socket) => {
     });
 
     socket.on('disconnect', () => {
-        console.log('User Had Left');
+        const user = removeUser(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left.` })
+        }
     });
 });
 
